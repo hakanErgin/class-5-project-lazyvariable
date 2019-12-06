42be1dc2bac5028be09fdfddf194e39f4be24473
hakanErgin <hakan.ergin@gmail.com>
2019-12-06 20:16:09 +0100

    added root route that lists users

--- src/lib/util.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {String} input
- * @returns {String} output in upper form
- */
-export default function toUpper(input) {
-  return input.toUpperCase()
-}
--- src/lib/util.test.js
+++
@@ -1,5 +0,0 @@
-import toUpper from "./util";
-
-it('converts to upper case', () => {
-  expect(toUpper("foo")).toBe('FOO')
-});
