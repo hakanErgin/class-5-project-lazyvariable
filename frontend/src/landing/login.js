86e11f7ac8a529caf688774724df003ad6676290
Ramzi Salem <ramzysa96@gmail.com>
2019-12-17 16:40:56 +0100

    features

--- frontend/src/landing/login.js
+++ frontend/src/landing/login.js
@@ -13,6 +13,9 @@ const LoginComponent = () => {
       .then(e => {
         if (e.data.token) {
           localStorage.setItem('token', e.data.token);
+          localStorage.setItem('username', e.data.user.username);
+          localStorage.setItem('ID', e.data.user.id);
+
           setLoggedIn(true);
           console.log(e.data.token);
         } else {
@@ -35,77 +38,75 @@ const LoginComponent = () => {
       {loggedIn ? (
         <Redirect to="/auth" />
       ) : (
-          <div>
-            <div className="headerSignUp">
-              <div className="leftSide">
-                <a href="/">
-                  <img src="https://i.ibb.co/cDXz5vG/logo.png" alt="logo" border="0" />
-                </a>
-                <div className="rightTop">
-                  <ul className="navMenu">
-                    <a href="/">
-                      {' '}
-                      <li className="menuItem">Home</li>
-                    </a>
-                    <li className="menuItem">About us</li>
-                    <li className="menuItem">Contact us</li>
-                  </ul>
-                  <button className="loginInButton">Login in</button>
-                  <a href="/signup">
-                    {' '}
-                    <button className="signUpButton">Get started</button>
+        <div>
+          <div className="headerSignUp">
+            <div className="leftSide">
+              <a href="/">
+                <img src="https://i.ibb.co/cDXz5vG/logo.png" alt="logo" border="0" />
+              </a>
+              <div className="rightTop">
+                <ul className="navMenu">
+                  <a href="/">
+                    <li className="menuItem">Home</li>
                   </a>
-                </div>
-                <div className="titleSection">
-                  <div className="subTitle">Easy and free!</div>
-                  <div className="title">
-                    Take the first step
-                  <br />
-                    into your future<span className="titleEndPoint">.</span>
-                  </div>
-                </div>
-              </div>
-              <div className="rightSide">
-                <form
-                  onSubmit={e => {
-                    e.preventDefault();
-                    submitForm();
-                  }}
-                >
+                  <li className="menuItem">About us</li>
+                  <li className="menuItem">Contact us</li>
+                </ul>
+                <button className="loginInButton">Login in</button>
+                <a href="/signup">
                   {' '}
-                  <div className="signUpText">Sign in</div>
-                  <div className="signUpLabel">Email</div>
-                  <input
-                    className="signUpInput"
-                    type="email"
-                    required
-                    onChange={handleEmailChange}
-                    value={email}
-                  ></input>
-                  <div className="signUpLabel">Password</div>
-                  <input
-                    className="signUpInput"
-                    type="password"
-                    required
-                    onChange={handlePasswordChange}
-                    value={password}
-                  ></input>
-                  <br></br>
-                  <button className="signUpSubmit" type="submit">
-                    Submit
-                </button>
-                </form>
+                  <button className="signUpButton">Get started</button>
+                </a>
+              </div>
+              <div className="titleSection">
+                <div className="subTitle">Easy and free!</div>
+                <div className="title">
+                  Take the first step
+                  <br />
+                  into your future<span className="titleEndPoint">.</span>
+                </div>
               </div>
             </div>
-
-            <footer>
-              <div className="footerContainer">
-                <img className="logoMin" src="https://i.ibb.co/jgJW3wx/logomin.png" alt="logomin" />
-                <div className="copyrightText">All rights are reserved</div>
-              </div>
-            </footer>
+            <div className="rightSide">
+              <form
+                onSubmit={e => {
+                  e.preventDefault();
+                  submitForm();
+                }}
+              >
+                <div className="signUpText">Sign in</div>
+                <div className="signUpLabel">Email</div>
+                <input
+                  className="signUpInput"
+                  type="email"
+                  required
+                  onChange={handleEmailChange}
+                  value={email}
+                ></input>
+                <div className="signUpLabel">Password</div>
+                <input
+                  className="signUpInput"
+                  type="password"
+                  required
+                  onChange={handlePasswordChange}
+                  value={password}
+                ></input>
+                <br></br>
+                <button className="signUpSubmit" type="submit">
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
-        )}
+
+          <footer>
+            <div className="footerContainer">
+              <img className="logoMin" src="https://i.ibb.co/jgJW3wx/logomin.png" alt="logomin" />
+              <div className="copyrightText">All rights are reserved</div>
+            </div>
+          </footer>
+        </div>
+      )}
     </div>
   );
 };
--- routes/createUser.js
+++ routes/createUser.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const auth = require('../middleware/auth');
-
 const User = require('../models/user.model');
 require('dotenv').config();
 
@@ -10,11 +9,21 @@ require('dotenv').config();
 // @desc    Register new user
 // @access  Public
 
+// List all the users
+router.route('/all').get((req, res) => {
+  User.find()
+    .then(info => res.json(info))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// New register
 router.post('/', (req, res) => {
-  const { name, email, password } = req.body;
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
 
   // Simple validation
-  if (!name || !email || !password) {
+  if (!username || !email || !password) {
     return res.status(400).json({ msg: 'Please enter all fields' });
   }
 
@@ -22,7 +31,11 @@ router.post('/', (req, res) => {
   User.findOne({ email }).then(user => {
     if (user) return res.status(400).json({ msg: 'User already exists' });
 
-    const newUser = new User({ email, password });
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
 
     // create salt & hash
     bcrypt.genSalt(10, (err, salt) => {
@@ -37,6 +50,7 @@ router.post('/', (req, res) => {
               user: {
                 id: user.id,
                 email: user.email,
+                username: user.username,
               },
             });
           });
@@ -53,6 +67,73 @@ router.get('/user', auth, (req, res) => {
   User.findById(req.user.id)
     .select('-password')
     .then(user => res.json(user));
+});
+
+// Entering the data for the first time
+router.route('/:id').post(auth, (req, res) => {
+  User.findByIdAndUpdate(req.params.id)
+    .then(info => {
+      info.phone = req.body.phone;
+      info.picture = req.body.picture;
+      info.country = req.body.country;
+      info.city = req.body.city;
+      info.website = req.body.website;
+      info.education = req.body.education;
+      info.workExperience = req.body.workExperience;
+      info.skill = req.body.skill;
+      info.certificate = req.body.certificate;
+      info.language = req.body.language;
+      info.project = req.body.project;
+      info
+        .save()
+        .then(() => res.json('Added successfully!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Entering Github data
+router.route('/github/:id').post(auth, (req, res) => {
+  User.findByIdAndUpdate(req.params.id)
+    .then(info => {
+      info.gitHub = req.body.gitHub;
+      info
+        .save()
+        .then(() => res.json('Added successfully!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// updating the data
+router.route('/update/:id').put(auth, (req, res) => {
+  User.findByIdAndUpdate(req.params.id)
+    .then(info => {
+      info.phone = req.body.phone;
+      info.picture = req.body.picture;
+      info.country = req.body.country;
+      info.city = req.body.city;
+      info.website = req.body.website;
+      info.education = req.body.education;
+      info.workExperience = req.body.workExperience;
+      info.skill = req.body.skill;
+      info.certificate = req.body.certificate;
+      info.language = req.body.language;
+      info.gitHub = req.body.gitHub;
+      info.project = req.body.project;
+      info
+        .save()
+        .then(() => res.json('Added successfully!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Getting specified data by ID
+router.route('/:id').get((req, res) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
