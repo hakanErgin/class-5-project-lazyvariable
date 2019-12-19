f6f215df516c25c1014f364790c0e0357fe87f25
hakanErgin <hakan.ergin@gmail.com>
2019-12-19 14:43:47 +0100

    fixed ref errors from browser console

--- frontend/src/authed/profile/personal.js
+++ frontend/src/authed/profile/personal.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Route, Link } from 'react-router-dom';
 import './customStyle.css';
 import { Form, Input, Button, Typography } from 'antd';
 const { Title } = Typography;
-const Personal = ({ setSelected, inputs, ref, handleSubmit, handleInputChange }) => {
+const Personal = ({ setSelected, inputs, handleSubmit, handleInputChange }) => {
   console.log('personal inputs', inputs);
 
   return (
