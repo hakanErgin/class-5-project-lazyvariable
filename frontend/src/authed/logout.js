0310be2d3db59aa06a1a46af8c2e2b06ed2709a5
Ramzi Salem <ramzysa96@gmail.com>
2019-12-20 00:33:42 +0100

    other-fix

--- frontend/src/authed/logout.js
+++ frontend/src/authed/logout.js
@@ -2,12 +2,15 @@ import React, { useEffect } from 'react';
 
 const LogoutComponent = () => {
   useEffect(() => {
-    localStorage.clear()
-    alert('Logged out!')
-    return () => { }
+    localStorage.clear()((window.location.href = 'http://localhost:3000'));
+    return () => {};
   });
   return (
-    <div><h1>Go to <a href="http://localhost:3000">home</a></h1></div>
-  )
+    <div>
+      <h1>
+        Go to <a href="http://localhost:3000">home</a>
+      </h1>
+    </div>
+  );
 };
 export default LogoutComponent;
