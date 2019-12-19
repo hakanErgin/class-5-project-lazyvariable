5788dc138de4fd16fee2e46816930037705d1b56
hakanErgin <hakan.ergin@gmail.com>
2019-12-19 19:43:01 +0100

    still ugly

--- frontend/src/landing/Preview/Preview.js
+++ frontend/src/landing/Preview/Preview.js
@@ -1,9 +1,8 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
+// import './Preview.css'; // here is where the magic happens
 
-import './Preview.css'; // here is where the magic happens
-
-import { Timeline, Icon } from 'antd';
+import { Timeline, Icon, Divider, Card, Col, Row } from 'antd';
 
 
 const useFetch = url => {
@@ -69,105 +68,153 @@ function Preview() {
     <div className="preview" style={{ textAlign: 'center' }}>
       <div className="prev profile_preview">
         <div className="prev-info personal-info">
-          <h2>Personal Info</h2>
-          <ul>
-            <li>
-              <h3>My name: </h3>
-              <p>{name}</p>
-            </li>
-            <li>
-              <h3>My email adress: </h3>
-              <p>{email}</p>
-            </li>
-            <li>
-              <h3>My telephone number: </h3>
-              <p>{phone}</p>
-            </li>
-            <li>
-              <h3>My city: </h3>
-              <p>{city}</p>
-            </li>
-            <li>
-              <h3>My country: </h3>
-              <p>{country}</p>
-            </li>
-          </ul>
+          <Divider><h2>Personal Info</h2></Divider>
+          <div style={{ background: '#72A0C1', padding: '30px' }}>
+            <Row type="flex" justify="center" align="middle" gutter={[16, 16]}>
+              <Col span={8}>
+                <Card title="My name:" bordered={false}>
+                  {name}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="My email adress" bordered={false}>
+                  {email}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="My telephone number:" bordered={false}>
+                  {phone}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="My city:" bordered={false}>
+                  {city}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="My country:" bordered={false}>
+                  {country}
+                </Card>
+              </Col>
+            </Row>
+          </div>
         </div>
-
         <div className="prev-info education-info">
-          <h2>Education Info</h2>
-          <ul>
-            <li>
-              <h3>Instituon: </h3>
-              <p>{institution}</p>
-            </li>
-            <li>
-              <h3>Field Of Study: </h3>
-              <p>{fieldOfStudy}</p>
-            </li>
-            <li>
-              <h3>Degree: </h3>
-              <p>{degree}</p>
-            </li>
-            <li>
-              <h3>Start Date: </h3>
-              <p>{educationStartDate}</p>
-            </li>
-            <li>
-              <h3>End Date: </h3>
-              <p>{educationEndDate}</p>
-            </li>
-          </ul>
+          <Divider><h2>Education Info</h2></Divider>
+          <div style={{ background: '#84DE02', padding: '30px' }}>
+            <Row type="flex" justify="center" align="middle" gutter={[16, 16]}>
+              <Col span={8}>
+                <Card title="Instituon:" bordered={false}>
+                  {institution}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Field Of Study:" bordered={false}>
+                  {fieldOfStudy}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Degree:" bordered={false}>
+                  {degree}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Start Date:" bordered={false}>
+                  {educationStartDate}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="End Date:" bordered={false}>
+                  {educationEndDate}
+                </Card>
+              </Col>
+            </Row>
+          </div>
         </div>
-
         <div className="prev-info experience-info">
-          <h2>Experience</h2>
-          <ul>
-            <li>
-              <h3>Title: </h3>
-              <p>{workTitle}</p>
-            </li>
-            <li>
-              <h3>Company: </h3>
-              <p>{companyName}</p>
-            </li>
-            <li>
-              <h3>Location Of Company: </h3>
-              <p>{companyLocation}</p>
-            </li>
-            <li>
-              <h3>Type of Employment : </h3>
-              <p>{employmentType}</p>
-            </li>
-            <li>
-              <h3>Start Date: </h3>
-              <p>{workStartDate}</p>
-            </li>
-            <li>
-              <h3>End Date: </h3>
-              <p>{workEndDate}</p>
-            </li>
-            <li>
-              <h3>Description of Job: </h3>
-              <p>{jobDescription}</p>
-            </li>
-          </ul>
+          <Divider><h2>Experience</h2></Divider>
+          <div style={{ background: '#E52B50', padding: '30px' }}>
+            <Row type="flex" justify="center" align="middle" gutter={[16, 16]}>
+              <Col span={8}>
+                <Card title="Title:" bordered={false}>
+                  {workTitle}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Company:" bordered={false}>
+                  {companyName}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Location Of Compan:" bordered={false}>
+                  {companyLocation}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Type of Employment:" bordered={false}>
+                  {employmentType}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Start Date:" bordered={false}>
+                  {workStartDate}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="End Date:" bordered={false}>
+                  {workEndDate}
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Description of Job:" bordered={false}>
+                  {jobDescription}
+                </Card>
+              </Col>
+            </Row>
+          </div>
         </div>
-
+        <div>
+          <Divider><h2>Timeline</h2></Divider>
+          <Timeline mode="alternate">
+            <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+            <Timeline.Item color="green">Solve initial network problems 2015-09-01</Timeline.Item>
+            <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+              laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+              beatae vitae dicta sunt explicabo.
+              </Timeline.Item>
+            <Timeline.Item color="red">Network problems being solved 2015-09-01</Timeline.Item>
+            <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+            <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>
+              Technical testing 2015-09-01
+              </Timeline.Item>
+          </Timeline>
+        </div>
         <div className="prev-info skills-info">
-          <h2>Skills</h2>
-          <ul>
-            <li>
-              <h3>My Skills: </h3>
-              <p>{skills}</p>
-            </li>
-          </ul>
+          <Divider><h2>Skills</h2></Divider>
+          <div style={{ background: '#4DA6FF', padding: '30px' }}>
+            <Row type="flex" justify="center" align="middle" gutter={[16, 16]}>
+              <Col span={8}>
+                <Card title="My Skills:" bordered={false}>
+                  {skills}
+                </Card>
+              </Col>
+            </Row>
+          </div>
         </div>
       </div>
-
       <div className="prev github_preview">
         <div className="prev-info">
-          <h2>Github Preview</h2>
+          <Divider><h2>Github Preview</h2></Divider>
+          <div style={{ background: '#FFBF00', padding: '30px' }}>
+            <Row type="flex" justify="center" align="middle" gutter={[16, 16]}>
+              <Col span={8}>
+                <Card title="Project:" bordered={false}>
+                  github
+                </Card>
+              </Col>
+            </Row>
+          </div>
           {/* <h3>{gitHubTitle1}</h3>
           <h3>{gitHubDescription1}</h3>
           <h3>{gitHubRepository1}</h3>
@@ -178,22 +225,6 @@ function Preview() {
           <h3>{gitHubDescription3}</h3>
           <h3>{gitHubRepository3}</h3> */}
         </div>
-      </div>
-      <div>
-      <Timeline mode="alternate">
-    <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-    <Timeline.Item color="green">Solve initial network problems 2015-09-01</Timeline.Item>
-    <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>
-      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-      laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-      beatae vitae dicta sunt explicabo.
-    </Timeline.Item>
-    <Timeline.Item color="red">Network problems being solved 2015-09-01</Timeline.Item>
-    <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-    <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>
-      Technical testing 2015-09-01
-    </Timeline.Item>
-  </Timeline>
       </div>
     </div>
   );
