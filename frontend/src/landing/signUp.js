4cd6d7308ee3a8f235ef06941ada7549571fd603
hakanErgin <hakan.ergin@gmail.com>
2019-12-30 09:21:47 +0100

    error handling for login signup

--- frontend/src/landing/signUp.js
+++ frontend/src/landing/signUp.js
@@ -11,6 +11,8 @@ const SignUp = () => {
   const [username, setUsername] = useState('');
   const [registered, setRegistered] = useState(false);
 
+  const [errorMessage, setErrorMessage] = useState('');
+
   const submitForm = () => {
     axios
       .post(`${HEROKU_URI}/user`, { username, email, password }) // BEFORE MERGE: .post('${process.env.HEROKU_URI}/user', { email, password, name })
@@ -26,9 +28,8 @@ const SignUp = () => {
           setRegistered(false);
         }
       })
-
       .catch(err => {
-        console.log(err);
+        setErrorMessage(err.response.statusText)
       });
   };
   const handleEmailChange = e => {
@@ -48,84 +49,85 @@ const SignUp = () => {
       {registered ? (
         <Redirect to="/auth/dashboard" />
       ) : (
-        <div>
-          <div className="headerSignUp">
-            <div className="leftSide">
-              <a href="/">
-                <img src="https://i.ibb.co/cDXz5vG/logo.png" alt="logo" border="0" />
-              </a>
-              <div className="rightTop">
-                <ul className="navMenu">
-                  <a href="/">
-                    <li className="menuItem">Home</li>
+          <div>
+            <div className="headerSignUp">
+              <div className="leftSide">
+                <a href="/">
+                  <img src="https://i.ibb.co/cDXz5vG/logo.png" alt="logo" border="0" />
+                </a>
+                <div className="rightTop">
+                  <ul className="navMenu">
+                    <a href="/">
+                      <li className="menuItem">Home</li>
+                    </a>
+                    <li className="menuItem">About us</li>
+                    <li className="menuItem">Contact us</li>
+                  </ul>
+                  <a href="/login">
+                    <button className="loginInButton">Log in</button>
                   </a>
-                  <li className="menuItem">About us</li>
-                  <li className="menuItem">Contact us</li>
-                </ul>
-                <a href="/login">
-                  <button className="loginInButton">Log in</button>
-                </a>
-                <a href="/signup">
-                  <button className="signUpButton">Get started</button>
-                </a>
-              </div>
-              <div className="titleSection">
-                <div className="subTitle">Easy and free!</div>
-                <div className="title">
-                  Take the first step
+                  <a href="/signup">
+                    <button className="signUpButton">Get started</button>
+                  </a>
+                </div>
+                <div className="titleSection">
+                  <div className="subTitle">Easy and free!</div>
+                  <div className="title">
+                    Take the first step
                   <br />
-                  into your future<span className="titleEndPoint">.</span>
+                    into your future<span className="titleEndPoint">.</span>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="rightSide">
-              <form
-                onSubmit={e => {
-                  e.preventDefault();
-                  submitForm();
-                }}
-              >
-                <div className="signupTitle">Sign up</div>
-                <div className="signUpLabel">GitHub username</div>
-                <input
-                  className="signUpInput"
-                  type="text"
-                  required
-                  onChange={handleUsernameChange}
-                  value={username}
-                ></input>
-                <div className="signUpLabel">Email</div>
-                <input
-                  className="signUpInput"
-                  type="email"
-                  required
-                  onChange={handleEmailChange}
-                  value={email}
-                ></input>
-                <div className="signUpLabel">Password</div>
-                <input
-                  className="signUpInput"
-                  type="password"
-                  required
-                  onChange={handlePasswordChange}
-                  value={password}
-                ></input>
-                <br></br>
-                <button className="signUpSubmit" type="submit">
-                  Submit
+              <div className="rightSide">
+                <form
+                  onSubmit={e => {
+                    e.preventDefault();
+                    submitForm();
+                  }}
+                >
+                  <div className="signupTitle">Sign up</div>
+                  <div className="signUpLabel">GitHub username to import your repositories</div>
+                  <input
+                    className="signUpInput"
+                    type="text"
+                    required
+                    onChange={handleUsernameChange}
+                    value={username}
+                  ></input>
+                  <div className="signUpLabel">Email</div>
+                  <input
+                    className="signUpInput"
+                    type="email"
+                    required
+                    onChange={handleEmailChange}
+                    value={email}
+                  ></input>
+                  <div className="signUpLabel">Password</div>
+                  <input
+                    className="signUpInput"
+                    type="password"
+                    required
+                    onChange={handlePasswordChange}
+                    value={password}
+                  ></input>
+                  <br />
+                  {errorMessage && <div style={{ paddingTop: "20px", color: "red" }}>{errorMessage}</div>}
+                  <button className="signUpSubmit" type="submit">
+                    Submit
                 </button>
-              </form>
+                </form>
+              </div>
             </div>
-          </div>
 
-          <footer>
-            <div className="footerContainer">
-              <img className="logoMin" src="https://i.ibb.co/jgJW3wx/logomin.png" alt="logomin" />
-              <div className="copyrightText">All rights are reserved</div>
-            </div>
-          </footer>
-        </div>
-      )}
+            <footer>
+              <div className="footerContainer">
+                <img className="logoMin" src="https://i.ibb.co/jgJW3wx/logomin.png" alt="logomin" />
+                <div className="copyrightText">All rights are reserved</div>
+              </div>
+            </footer>
+          </div>
+        )}
     </div>
   );
 };
