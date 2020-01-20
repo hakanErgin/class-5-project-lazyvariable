a4b69dfc76ecd557218030a97ea47465c2bec01e
hakanErgin <hakan.ergin@gmail.com>
2020-01-20 19:45:00 +0100

    fix uris

--- frontend/src/helpers/firebaseHelper.js
+++ frontend/src/helpers/firebaseHelper.js
@@ -1,2 +1,2 @@
-const REACT_APP_FIREBASE_URI = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.REACT_APP_FIREBASE_URI;
+const REACT_APP_FIREBASE_URI = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://portfolio-d9052.firebaseapp.com';
 export default REACT_APP_FIREBASE_URI
--- frontend/src/helpers/herokuHelper.js
+++ frontend/src/helpers/herokuHelper.js
@@ -1,2 +1,2 @@
-const REACT_APP_BACKEND_URI = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : process.env.REACT_APP_BACKEND_URI;
+const REACT_APP_BACKEND_URI = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://mighty-reaches-37532.herokuapp.com';
 export default REACT_APP_BACKEND_URI
