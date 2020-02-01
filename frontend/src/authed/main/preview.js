6081119c006d685d585bc31cd8743ee5f0fb539d
hakanErgin <hakan.ergin@gmail.com>
2020-02-01 15:46:03 +0100

    wip cards

--- frontend/src/authed/main/preview.js
+++ frontend/src/authed/main/preview.js
@@ -1,21 +1,12 @@
 import React from "react"
 import { useState, useEffect } from "react"
-
-import REACT_APP_BACKEND_URI from "../../helpers/herokuHelper"
 import fields from "./profile/handlers/fieldData"
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {
-  faPhoneAlt,
-  faEnvelope,
-  faMapMarkerAlt,
-  faGlobeEurope
-} from "@fortawesome/free-solid-svg-icons"
+import cardData from "./profile/handlers/cardData"
 
 import { Grid, Row, Col } from "react-flexbox-grid"
-import { Card } from "antd"
-
+import { Card, Icon, Tooltip } from "antd"
 import "../../styles/preview.css"
+const { Meta } = Card
 
 const [personalFields, experienceFields, educationFields, numbers] = fields.map(
   e => e[Object.keys(e)]
@@ -56,36 +47,41 @@ const Preview = ({ avatar, inputs, CheckDb }) => {
     gitHub
   } = inputs
 
+  const { metaCard } = cardData(
+    name,
+    email,
+    about,
+    website,
+    phone,
+    country,
+    city,
+    workTitle,
+    companyName,
+    companyLocation,
+    employmentType,
+    jobDescription,
+    workStartDate,
+    workEndDate,
+    institution,
+    fieldOfStudy,
+    degree,
+    educationDescription,
+    educationStartDate,
+    educationEndDate,
+    skills,
+    gitHub
+  )
+
   console.log(inputs)
 
   return (
     <div id="preview">
-      <Grid fluid className="personalField1">
-        <img src={avatar.avatar} alt={"avatar"} />
-        <h1>{name}</h1>
-        <h2>{workTitle}</h2>
-        <Row className="personalField1">
-          <Col>
-            <FontAwesomeIcon icon={faMapMarkerAlt} size="2x" color="#082371" />
-            <div className="info">
-              {city}, {country}
-            </div>
-          </Col>
-          <Col>
-            <FontAwesomeIcon icon={faPhoneAlt} size="2x" color="#082371" />
-            <div className="info">{phone}</div>
-          </Col>
-          <Col>
-            <FontAwesomeIcon icon={faEnvelope} size="2x" color="#082371" />
-            <div className="info">{email}</div>
-          </Col>
-          <Col>
-            <FontAwesomeIcon icon={faGlobeEurope} size="2x" color="#082371" />
-            <div className="info">{website}</div>
-          </Col>
-        </Row>
-        <Col className="about">{about}</Col>
-      </Grid>
+      <div>{metaCard(avatar)}</div>
+      <Card title="cARrd" extra={<a href="#">More</a>} style={{ width: 300 }}>
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
     </div>
   )
 }
