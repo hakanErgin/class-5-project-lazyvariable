073936da48746985e1b428605f9e3c0d2a0f2d77
hakanErgin <hakan.ergin@gmail.com>
2020-02-09 16:33:13 +0100

    button, sider and css update

--- frontend/src/authed/main/profile.js
+++ frontend/src/authed/main/profile.js
@@ -59,63 +59,61 @@ const Profile = () => {
   }
 
   return (
-    <div>
-      <div>
-        <div className="header">
-          <Title level={1}>Profile</Title>
-          <div>Take your first step! Create your resume</div>
-        </div>
-        <Menu
-          id="profile-menu"
-          selectedKeys={activeTab}
-          onClick={handleClick}
-          mode="horizontal"
-        >
-          <Menu.Item key="1">
-            <Link to="/auth/profile/personal">Personal Info</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/auth/profile/experience">Experience</Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Link to="/auth/profile/education">Education</Link>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Link to="/auth/profile/skills">Skills</Link>
-          </Menu.Item>
-        </Menu>
-        <Switch>
-          <Route path="/auth/profile/personal">
-            <Personal
-              inputs={inputs.personalFields}
-              handleInputChange={handlePersonalInputChange}
-            />
-          </Route>
-          <Route path="/auth/profile/experience">
-            <Experience
-              inputs={inputs.experienceFields}
-              handleInputChange={handleInputChange}
-              onDateChange={onDateChange}
-            />
-          </Route>
-          <Route path="/auth/profile/education">
-            <Education
-              inputs={inputs.educationFields}
-              handleInputChange={handleInputChange}
-              onDateChange={onDateChange}
-            />
-          </Route>
-          <Route path="/auth/profile/skills">
-            <Skills
-              inputs={inputs.skills}
-              handleSkillsInputChange={handleSkillsInputChange}
-            />
-          </Route>
-        </Switch>
-        <footer>
-          <Button onClick={nextHandler}>{buttonText}</Button>
-        </footer>
+    <div id="profileComponent">
+      <div className="header">
+        <Title level={1}>Profile</Title>
+        <div>Take your first step! Create your resume</div>
       </div>
+      <Menu
+        id="profile-menu"
+        selectedKeys={activeTab}
+        onClick={handleClick}
+        mode="horizontal"
+      >
+        <Menu.Item key="1">
+          <Link to="/auth/profile/personal">Personal Info</Link>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Link to="/auth/profile/experience">Experience</Link>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Link to="/auth/profile/education">Education</Link>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <Link to="/auth/profile/skills">Skills</Link>
+        </Menu.Item>
+      </Menu>
+      <Switch>
+        <Route path="/auth/profile/personal">
+          <Personal
+            inputs={inputs.personalFields}
+            handleInputChange={handlePersonalInputChange}
+          />
+        </Route>
+        <Route path="/auth/profile/experience">
+          <Experience
+            inputs={inputs.experienceFields}
+            handleInputChange={handleInputChange}
+            onDateChange={onDateChange}
+          />
+        </Route>
+        <Route path="/auth/profile/education">
+          <Education
+            inputs={inputs.educationFields}
+            handleInputChange={handleInputChange}
+            onDateChange={onDateChange}
+          />
+        </Route>
+        <Route path="/auth/profile/skills">
+          <Skills
+            inputs={inputs.skills}
+            handleSkillsInputChange={handleSkillsInputChange}
+          />
+        </Route>
+      </Switch>
+      <footer>
+        <Button onClick={nextHandler}>{buttonText}</Button>
+      </footer>
     </div>
   );
 };
